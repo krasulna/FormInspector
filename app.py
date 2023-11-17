817072bfe084bac0bf54d65b7f2501c04e20e283
krasulna <krasulna@gmail.com>
2023-11-17 16:15:25 +0200

    end of develop (17.11)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Dict
 
 from fastapi import FastAPI, Form, HTTPException
 from pydantic import BaseModel
@@ -7,31 +8,32 @@ import json
 import uvicorn
 
 app = FastAPI()
-db = TinyDB('db.json')
+db = TinyDB('db/db.json')
+
 
 class FormData(BaseModel):
-    f_name1: str
-    f_name2: str
+    data: Dict[str, str]
+
 
 def initialize_database():
-    with open('test_data.json') as file:
+    with open('db/test_data.json') as file:
         data = json.load(file)
         db.insert_multiple(data)
+
 
 def find_matching_template(data):
     templates = db.all()
     for template in templates:
-        template_values = set(template.values())
         template_name = template.get('name')
-        data_values = set(determine_field_type(value) for value in data.values())
-        if template_name and template_values == data_values:
+        template_values = {k: v for k, v in template.items() if k != 'name'}
+        data_values = {k: determine_field_type(v) for k, v in data.items()}
+
+        # Проверяем, что все значения ключей ШАБЛОНА подходят под значения валидации ЗАПРОСА
+        if all(data_values[data_key] == template_values[template_key] for template_key, data_key in
+               zip(template_values.keys(), data_values.keys())):
             return template_name
+
     return None
-
-
-
-
-
 
 
 def determine_field_type(value):
@@ -40,23 +42,28 @@ def determine_field_type(value):
             return "email"
         elif value.startswith("+7") and value[1:].replace(" ", "").isdigit() and len(value) == 12:
             return "phone"
-        elif "." in value and "-" in value:
+        elif "." in value or "-" in value:
             try:
                 datetime.datetime.strptime(value, "%Y-%m-%d")
                 return "date"
             except ValueError:
-                pass
+                try:
+                    datetime.datetime.strptime(value, "%d.%m.%Y")
+                    return "date"
+                except ValueError:
+                    return "text"
         else:
             return "text"
     return "text"
 
+
 @app.post("/get_form")
 async def get_form(data: FormData):
-    matching_template = find_matching_template(data.model_dump())
+    matching_template = find_matching_template(data.data)
     if matching_template:
         return {"template_name": matching_template}
     else:
-        field_types = {field_name: determine_field_type(value) for field_name, value in data.model_dump().items()}
+        field_types = {field_name: determine_field_type(value) for field_name, value in data.data.items()}
         return field_types
 
 if __name__ == '__main__':
